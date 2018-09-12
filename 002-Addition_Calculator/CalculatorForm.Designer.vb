77f299f29459ada30492d56937bba7840229546b
afewvowels <keithbsmith87@gmail.com>
2018-09-12 14:30:31 -0400

    initial commit

---
+++ 002-Addition_Calculator/CalculatorForm.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalculatorForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FirstIntegerLabel = New System.Windows.Forms.Label()
+        Me.SecondIntegerLabel = New System.Windows.Forms.Label()
+        Me.FirstTextBox = New System.Windows.Forms.TextBox()
+        Me.SecondTextBox = New System.Windows.Forms.TextBox()
+        Me.AddButton = New System.Windows.Forms.Button()
+        Me.SolutionLabel = New System.Windows.Forms.Label()
+        Me.AnswerTextBox = New System.Windows.Forms.TextBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'FirstIntegerLabel
+        '
+        Me.FirstIntegerLabel.Location = New System.Drawing.Point(12, 9)
+        Me.FirstIntegerLabel.Name = "FirstIntegerLabel"
+        Me.FirstIntegerLabel.Size = New System.Drawing.Size(114, 23)
+        Me.FirstIntegerLabel.TabIndex = 0
+        Me.FirstIntegerLabel.Text = "First Integer:"
+        '
+        'SecondIntegerLabel
+        '
+        Me.SecondIntegerLabel.Location = New System.Drawing.Point(132, 9)
+        Me.SecondIntegerLabel.Name = "SecondIntegerLabel"
+        Me.SecondIntegerLabel.Size = New System.Drawing.Size(140, 23)
+        Me.SecondIntegerLabel.TabIndex = 1
+        Me.SecondIntegerLabel.Text = "Second Integer:"
+        '
+        'FirstTextBox
+        '
+        Me.FirstTextBox.Location = New System.Drawing.Point(13, 36)
+        Me.FirstTextBox.Name = "FirstTextBox"
+        Me.FirstTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.FirstTextBox.TabIndex = 2
+        '
+        'SecondTextBox
+        '
+        Me.SecondTextBox.Location = New System.Drawing.Point(135, 35)
+        Me.SecondTextBox.Name = "SecondTextBox"
+        Me.SecondTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.SecondTextBox.TabIndex = 3
+        '
+        'AddButton
+        '
+        Me.AddButton.Location = New System.Drawing.Point(15, 63)
+        Me.AddButton.Name = "AddButton"
+        Me.AddButton.Size = New System.Drawing.Size(220, 23)
+        Me.AddButton.TabIndex = 4
+        Me.AddButton.Text = "Add"
+        Me.AddButton.UseVisualStyleBackColor = True
+        '
+        'SolutionLabel
+        '
+        Me.SolutionLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SolutionLabel.Location = New System.Drawing.Point(15, 128)
+        Me.SolutionLabel.Name = "SolutionLabel"
+        Me.SolutionLabel.Size = New System.Drawing.Size(222, 23)
+        Me.SolutionLabel.TabIndex = 5
+        '
+        'AnswerTextBox
+        '
+        Me.AnswerTextBox.Location = New System.Drawing.Point(15, 155)
+        Me.AnswerTextBox.Name = "AnswerTextBox"
+        Me.AnswerTextBox.ReadOnly = True
+        Me.AnswerTextBox.Size = New System.Drawing.Size(222, 20)
+        Me.AnswerTextBox.TabIndex = 6
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(15, 92)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(220, 23)
+        Me.ClearButton.TabIndex = 7
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'CalculatorForm
+        '
+        Me.AcceptButton = Me.AddButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ClearButton
+        Me.ClientSize = New System.Drawing.Size(247, 187)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.AnswerTextBox)
+        Me.Controls.Add(Me.SolutionLabel)
+        Me.Controls.Add(Me.AddButton)
+        Me.Controls.Add(Me.SecondTextBox)
+        Me.Controls.Add(Me.FirstTextBox)
+        Me.Controls.Add(Me.SecondIntegerLabel)
+        Me.Controls.Add(Me.FirstIntegerLabel)
+        Me.Name = "CalculatorForm"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FirstIntegerLabel As System.Windows.Forms.Label
+    Friend WithEvents SecondIntegerLabel As System.Windows.Forms.Label
+    Friend WithEvents FirstTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents SecondTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents AddButton As System.Windows.Forms.Button
+    Friend WithEvents SolutionLabel As System.Windows.Forms.Label
+    Friend WithEvents AnswerTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ClearButton As System.Windows.Forms.Button
+
+End Class
---
+++ 002-Addition_Calculator/CalculatorForm.vb
@@ -0,0 +1,82 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class CalculatorForm
+
+    Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
+        'Dim IntOne As Integer = FirstTextBox.Text
+        'Dim IntTwo As Integer = SecondTextBox.Text
+        'Dim IntSum As Integer = IntOne + IntTwo
+
+        'SolutionLabel.Text = IntSum
+
+        Dim ErrorString As String
+
+        ' Try to cast textbox contents to integer values and add them,
+        ' display error message to user if non-integer values are detected
+        Try
+            ' Single-line solutions
+            ' Dim AnswerString As String = Convert.ToString(Convert.ToInt16(FirstTextBox.Text) + Convert.ToInt16(SecondTextBox.Text))
+            ' Dim AnswerString As String = Convert.ToString(Integer.Parse(FirstTextBox.Text) + Integer.Parse(SecondTextBox.Text))
+
+            ' Variable declarations
+            Dim FirstInteger As Integer ' Represents the first value
+            Dim SecondInteger As Integer ' Represents the second value
+            Dim SumInteger As Integer ' Holds the sum of the two integer variables
+
+            ' Parse variables
+            FirstInteger = Integer.Parse(FirstTextBox.Text)
+
+            Try
+                SecondInteger = Integer.Parse(SecondTextBox.Text)
+
+                ' Add two integers
+                SumInteger = FirstInteger + SecondInteger
+
+                SolutionLabel.Text = "The sum is " & SumInteger.ToString
+                AnswerTextBox.Text = "The sum is " & SumInteger.ToString
+            Catch ex As FormatException
+                If SecondTextBox.Text Is String.Empty Then
+                    ErrorString = "The second value was empty."
+                Else
+                    ErrorString = "The second value was a non-integer value."
+                End If
+
+                'SolutionLabel.Text = ErrorString
+                'AnswerTextBox.Text = ErrorString
+
+                MessageBox.Show(ErrorString,
+                                "Data Entry Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation)
+                SecondTextBox.Focus()
+                SecondTextBox.SelectAll()
+            End Try
+        Catch ex As FormatException
+            If FirstTextBox.Text Is String.Empty Then
+                ErrorString = "The first value was empty."
+            Else
+                ErrorString = "The first value was a non-integer value."
+            End If
+
+            'SolutionLabel.Text = ErrorString
+            'AnswerTextBox.Text = ErrorString
+            MessageBox.Show(ErrorString,
+                            "Data Entry Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation)
+
+            FirstTextBox.Focus()
+            FirstTextBox.SelectAll()
+            ' Finally
+            ' Regardless of success of operation, reset First and Second Text entry forms
+            ' FirstTextBox.Text = ""
+            ' SecondTextBox.Text = ""
+        End Try
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        FirstTextBox.Text = ""
+        SecondTextBox.Text = ""
+    End Sub
+End Class
